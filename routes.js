42c64e14438c5b489deb5c2afc532e48fc9b3123
odedex <karitvruda@hotmail.com>
2016-06-29 09:16:24 +0300

    added some code to conver GGA to latlng

--- routes.js
+++ routes.js
@@ -26,8 +26,29 @@ module.exports = function (app, io) {
                     res.end();
                 }
             })
+        } else {
+            res.write("bad params. format is /update/:id/:timestamp/:lat/:lng");
+            res.end();
         }
     });
+    
+    //TODO: http://stackoverflow.com/questions/1140189/converting-latitude-and-longitude-to-decimal-values
+    //TODO: update this
+    function ParseDMS(input) {
+        var parts = input.split(/[^\d\w]+/);
+        var lat = ConvertDMSToDD(parts[0], parts[1], parts[2], parts[3]);
+        var lng = ConvertDMSToDD(parts[4], parts[5], parts[6], parts[7]);
+    }
+
+    //TODO: update this
+    function ConvertDMSToDD(degrees, minutes, seconds, direction) {
+        var dd = degrees + minutes/60 + seconds/(60*60);
+
+        if (direction == "S" || direction == "W") {
+            dd = dd * -1;
+        } // Don't do anything for N or E
+        return dd;
+    }
 
     var gpsio = io.on('connection', function(socket) {
         socket.on('queryGPSID', function (id) {
